6ae1897a9b3fae896b2879f485274483c9a39667
Amru-1357 <amratharao@gmail.com>
2025-04-22 13:41:51 -0700

    updated files

---
+++ src/User.java
@@ -0,0 +1,12 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class User {
+    private final String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+}
