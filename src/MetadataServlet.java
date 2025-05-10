38edc52b28d7c1bd1187bf9700720d526757a709
aashiryar <raiar@uci.edu>
2025-05-10 16:45:21 -0700

    metadata loading too - just login fix left

--- src/MetadataServlet.java
+++ src/MetadataServlet.java
@@ -1,4 +1,4 @@
-package servlets;
+//package servlets;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -7,66 +7,57 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(name = "MetadataServlet", urlPatterns = "/api/metadata")
 public class MetadataServlet extends HttpServlet {
     private DataSource dataSource;
 
-    @Override
     public void init() throws ServletException {
         try {
-            dataSource = (DataSource)
-                    new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
         } catch (NamingException e) {
             throw new ServletException(e);
         }
     }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        JsonObject result    = new JsonObject();
-        JsonArray  tablesArr = new JsonArray();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JsonObject result = new JsonObject();
+        JsonArray tablesArray = new JsonArray();
 
         try (Connection conn = dataSource.getConnection()) {
             DatabaseMetaData meta = conn.getMetaData();
-
-            // either hard‐code your DB name:
-            String myDb = "moviedb";
-
-            try (ResultSet tables = meta.getTables(myDb, null, "%", new String[]{"TABLE"})) {
-                while (tables.next()) {
-                    String tableName = tables.getString("TABLE_NAME");
-
-                    JsonObject tableObj = new JsonObject();
-                    tableObj.addProperty("name", tableName);
-
-                    JsonArray colsArr = new JsonArray();
-                    try (ResultSet cols = meta.getColumns(myDb, null, tableName, "%")) {
-                        while (cols.next()) {
-                            JsonObject col = new JsonObject();
-                            col.addProperty("name", cols.getString("COLUMN_NAME"));
-                            col.addProperty("type", cols.getString("TYPE_NAME"));
-                            colsArr.add(col);
-                        }
-                    }
-
-                    tableObj.add("columns", colsArr);
-                    tablesArr.add(tableObj);
+            ResultSet tables = meta.getTables(null, null, "%", new String[]{"TABLE"});
+            while (tables.next()) {
+                String tableName = tables.getString("TABLE_NAME");
+                JsonObject tableObj = new JsonObject();
+                tableObj.addProperty("name", tableName);
+                JsonArray columnsArray = new JsonArray();
+                ResultSet columns = meta.getColumns(null, null, tableName, "%");
+                while (columns.next()) {
+                    JsonObject col = new JsonObject();
+                    col.addProperty("name", columns.getString("COLUMN_NAME"));
+                    col.addProperty("type", columns.getString("TYPE_NAME"));
+                    columnsArray.add(col);
                 }
+                tableObj.add("columns", columnsArray);
+                tablesArray.add(tableObj);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        result.add("tables", tablesArr);
+        result.add("tables", tablesArray);
         response.setContentType("application/json");
         response.getWriter().write(result.toString());
     }
-
 }
